3dab976b5d873051bf29c52f7d9b30ceb3b9a406
icecreamsoldier <mariana.9000@hotmail.com>
2017-04-18 13:58:25 -0500

    Subtitle correction /

--- visualfest/web/static/js/components/Subtitle.jsx
+++ visualfest/web/static/js/components/Subtitle.jsx
@@ -7,11 +7,15 @@ export default class Subtitle extends Component{
       <div className = "subtitleLine">
       <div className = "containerSubtitle">
         <div className = "subtitleFormat">
+          <div className = "marginSubtitle">
           <h3 className = "white">PIENSA,</h3>
+          </div>
           <h3 className ="yellow">/</h3>
         </div>
         <div className = "subtitleFormat">
+          <div className = "marginSubtitle">
           <h3 className = "white">INTERACTÚA</h3>
+        </div>
           <h3 className ="yellow">TRANSFORMA</h3>
         </div>
         </div>
