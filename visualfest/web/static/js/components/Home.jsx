69f25a294c54985d56f8ccc892d876df8bb77a52
icecreamsoldier <mariana.9000@hotmail.com>
2017-04-18 10:36:53 -0500

    added margin left

--- visualfest/web/static/js/components/Home.jsx
+++ visualfest/web/static/js/components/Home.jsx
@@ -7,10 +7,12 @@ import Subtitle from '../components/Subtitle.jsx';
 export default class Home extends Component {
   render () {
     return (
+      <div className = "borderMain">
       <div className = "bodyMain">
       <Title yellowTitle = { this.props.yellowTitle } pinkTitle = { this.props.pinkTitle }/>
       <Subtitle/>
       <TextBox text={  this.props.bodyText } />
+    </div>
     </div>
   );
   }
