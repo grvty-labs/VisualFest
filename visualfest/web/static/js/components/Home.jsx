a67277a8f9bf4223189dbdc66246661c3e33a79b
icecreamsoldier <mariana.9000@hotmail.com>
2017-04-17 22:02:47 -0500

    Added colours and format

--- visualfest/web/static/js/components/Home.jsx
+++ visualfest/web/static/js/components/Home.jsx
@@ -2,12 +2,15 @@
 import React, { Component } from 'react';
 import TextBox from '../components/TextBox.jsx';
 import Title from '../components/Title.jsx';
+import Subtitle from '../components/Subtitle.jsx';
 
 export default class Home extends Component {
   render () {
-    return (<div className = "bodyMain">
+    return (
+      <div className = "bodyMain">
       <Title yellowTitle = { this.props.yellowTitle } pinkTitle = { this.props.pinkTitle }/>
-      <TextBox text={ this.props.bodyText } />
-    </div>);
+      <TextBox text={  this.props.bodyText } />
+    </div>
+  );
   }
 }
