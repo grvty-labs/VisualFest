99fd947871df5bf9e0b54594bfa802df67e65ce3
icecreamsoldier <mariana.9000@hotmail.com>
2017-04-18 11:11:17 -0500

    Subtitle margins

--- visualfest/web/static/js/components/Subtitle.jsx
+++ visualfest/web/static/js/components/Subtitle.jsx
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 export default class Subtitle extends Component{
   render() {
     return (
-      <div className = "containerSubtitleAll">
       <div className = "containerSubtitle">
         <div className = "subtitleFormat">
           <h3 className = "white">PIENSA,</h3>
@@ -14,7 +13,7 @@ export default class Subtitle extends Component{
           <h3 className = "white">INTERACTÚA</h3>
           <h3 className ="yellow">TRANSFORMA</h3>
         </div>
-      </div>
+        <div className = "subtitleLine"></div>
       </div>
     );
   }
