12ffbefc9364190e7f5070d9a173d66e32e9b045
icecreamsoldier <mariana.9000@hotmail.com>
2017-04-20 12:35:18 -0500

    Added functionality gallery

--- visualfest/web/static/js/components/ImagesGallery.jsx
+++ visualfest/web/static/js/components/ImagesGallery.jsx
@@ -2,10 +2,25 @@
 import React, { Component } from 'react';
 
 export default class ImagesGallery extends Component{
+  constructor(props) {
+    super(props);
+    this.changeSize = this.changeSize.bind(this);
+    this.state = {
+      selected: -1,
+    };
+  }
+
+  changeSize (index) {
+    this.setState({ selected: index });
+  }
+
   render() {
-    let images = this.props.images.map(function (image, index) {
+    let images = this.props.images.map((image, index) => {
       return (
-        <img key = { index } src = { image.src } className = "imagesGallery"/>
+        <img key = { index } src = { image.src }
+          className = { this.state.selected == index
+            ? 'bigImageGallery' : 'imagesGallery' }
+          onClick = { () => this.changeSize(index)} />
       );
     }
   );
